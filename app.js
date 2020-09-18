d500310461743fcd4f26d8a8396617e90d566d21
Your Name <gourav.saini.2703@gmail.com>
2020-09-18 18:14:41 +0200

    app.get

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const app = express();
 const port = process.env.port || 4000;
 app.listen(port, () => {console.log("worked");});
 //app.use(express.static('./public'));
-app.get("This");
+app.get("/", (req, res)=>{
+      res.send("Works");
+
+}  );
