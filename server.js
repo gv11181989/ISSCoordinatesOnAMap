7dfb92d5fbdca47fba47bc43cdc1ddd59ce673c0
Your Name <gourav.saini.2703@gmail.com>
2020-09-25 15:44:46 +0200

    node.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.port || 80;
 app.listen(port, () => {console.log("port 4000 active");});
 //app.use(express.static('./public'));
 app.get("/", (req, res)=>{
